306cbbb2e4181b57e1ece4ca34d9914596274d88
Mae Kangsadan <maemeier@live.com>
2019-12-11 18:33:11 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>JUST-IN-CASE</h1>
+    </div>
+  );
+}
+
+export default App;
